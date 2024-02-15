1754970973fb892f878b0ff2d42bb2e7692acdfe
RIYAZ AHAMED <riyazahamad397@gmail.com>
2024-02-15 12:19:54 +0530

    added all functionalities

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23568f09481c73e7"; }
+/******/ 		__webpack_require__.h = function() { return "26231c58580ce36a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,21 +1,68 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../component/layout";
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../component/RequestRow";
 class requestIndex extends Component {
   static async getInitialProps(props) {
+    console.log("h");
     const { address } = props.query;
-    return { address };
+    console.log(address);
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
+    console.log(requestCount);
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    console.log(requests);
+
+    return { address, requests, requestCount, approversCount };
+  }
+  renderRow() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Request</h3>
         <Link route={`/campaign/${this.props.address}/request/new`}>
           <a>
-            <Button primary> Add request</Button>
+            <Button primary floated="right" style={{marginBottom : 10}}> Add request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>DESCRIPTION</HeaderCell>
+              <HeaderCell>AMOUNT</HeaderCell>
+              <HeaderCell>RECIPIENT</HeaderCell>
+              <HeaderCell>APPROVAL</HeaderCell>
+              <HeaderCell>APPROVE</HeaderCell>
+              <HeaderCell>FINALIZE</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRow()}</Body>
+        </Table>
+        <div>Found {this.props.requestCount} Requests</div>
       </Layout>
     );
   }
